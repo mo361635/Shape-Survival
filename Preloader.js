d93b6ce3ad64a06ef31d8da275c4ff8e007fdf66
Oswaldo <munozbonifacioo@go.lisd.net>
2018-03-07 14:45:38 -0600

    Files

--- Preloader.js
+++ Preloader.js
@@ -31,7 +31,7 @@ BasicGame.Preloader.prototype = {
         
 		//	+ lots of other required assets here
         
-        this.load.image('background', 'images/background.png')
+        this.load.image('background', 'images/background.png');
 	},
 
 	create: function () {
